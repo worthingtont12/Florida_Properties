05f6f068448e09fc2fc63a7cd0b0b30ff401eca4
worthingtont12 <worthingtont12@gmail.com>
2016-10-31 08:33:25 -0400

    updating

--- Loading_Data.py
+++ Loading_Data.py
@@ -1,13 +1,19 @@
 import pandas as pd
-# df = pd.read_stata('/Volumes/Seagate Backup Plus Drive/NAL/NAL2014/nal11rts2014.dta')
-# print(df.columns.values)
-
 #Import all data files for the year
 fulldf = pd.DataFrame()
-listdf = []
+yeardf = []
 for i in range(11, 78):
     df = pd.read_stata('/Volumes/Seagate Backup Plus Drive/NAL/NAL2014/nal'+str(i)+'rts2014.dta',convert_categoricals=False, convert_missing=True)
     df['County_Code'] = i
-    listdf.append(df)
-
-fulldf = pd.concat(listdf)
+    yeardf.append(df)
+#
+# fulldf = pd.concat(listdf)
+# #Import all years
+# for j in range(1995, 2014):
+#     for i in range(11, 78):
+#         df = pd.read_stata('/Volumes/Seagate Backup Plus Drive/NAL/NAL'+str(j)+'/nal'+str(i)+'rts2014.dta',convert_categoricals=False, convert_missing=True)
+#         df['County_Code'] = i
+#         df['File_Year'] = j
+#         listdf.append(df)
+#
+# fulldf = pd.concat(listdf)
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Finding Trends amongst individual parcels in Florida 1995-2014.
 
 #Data
-The data contains information on all individual properties at the parcel level in Florida from 1995-2014. Each year is about 6GB and the total dataset is 90GB. The dataset contains all 67 counties and has information about the properties use and assessed value.
+The data contains information on all individual properties at the parcel level in Florida from 1995-2014. Each year is about 6GB and the total dataset is 90GB. The dataset contains all 67 counties and has information about the property's use and assessed value.
 
 #Collaborators
 Tyler Worthington and Lev Zadvinskiy
