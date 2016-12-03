16decf7cbea3b7b2c011a1291d668f16aa83fba3
Markgs22 <mrr4kj@virginia.edu>
2016-12-03 15:42:13 -0500

    Update Modeling.py

--- Data Cleaning/Modeling.py
+++ Data Cleaning/Modeling.py
@@ -3,6 +3,9 @@
 import statsmodels.formula.api as sm
 from patsy import dmatrices
 
+#Create residential-only dataset for modeling
+df_mod = dfTest.loc[dfTest['residential'] == True]
+
 #Model structure [UPDATE with appropriate / new variables as needed]
 y1, X1 = dmatrices('diff_btwn_prc_jv ~ Blk_Val + const_class + eff_yr_blt + imp_qual + lnd_sqfoot + sale_season1 + tot_lvg_area + no_buldng', data=dfTest, return_type='dataframe')
 mod1 = sm.OLS(y1, X1)
