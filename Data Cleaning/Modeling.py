b0582d11f4841919e69c8087ef9e0c3a991b48f5
Markgs22 <mrr4kj@virginia.edu>
2016-12-03 14:48:24 -0500

    Update Modeling.py

--- Data Cleaning/Modeling.py
+++ Data Cleaning/Modeling.py
@@ -4,7 +4,13 @@ import statsmodels.formula.api as sm
 from patsy import dmatrices
 
 #Model structure [UPDATE with appropriate / new variables as needed]
-y1, X1 = dmatrices('diff_btwn_prc_jv ~ Blk_Val + const_class + eff_yr_blt + imp_qual + lnd_sqfoot + sale_season1 + tot_lvg_area + no_buldng', data=dfhd, return_type='dataframe')
+y1, X1 = dmatrices('diff_btwn_prc_jv ~ Blk_Val + const_class + eff_yr_blt + imp_qual + lnd_sqfoot + sale_season1 + tot_lvg_area + no_buldng', data=dfTest, return_type='dataframe')
 mod1 = sm.OLS(y1, X1)
 mod_1 = mod1.fit()
 print(mod_1.summary())
+
+#2nd iteration: including JV as predictor
+y1, X2 = dmatrices('diff_btwn_prc_jv ~ jv + Blk_Val + const_class + eff_yr_blt + imp_qual + lnd_sqfoot + sale_season1 + tot_lvg_area + no_buldng', data=dfTest, return_type='dataframe')
+mod2 = sm.OLS(y1, X2)
+mod_2 = mod2.fit()
+print(mod_2.summary())
